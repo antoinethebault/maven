92f227a0efc622254370718225cd4f36e3fa818a
antoinethebault <antoine.thebau@gmail.com>
2020-02-26 14:20:07 +0100

    5e commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -7,7 +9,9 @@ package dev;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	
+    	String asciiArt = FigletFont.convertOneLine("App");
+    	System.out.println(asciiArt);
         System.out.println( "Hello World!" );
     }
 }
